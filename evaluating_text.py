8216e4c37f9fe7d3e2287a0fbbd0c27cd42712da
Elizar54 <y.elizarev@g.nsu.ru>
2023-09-07 12:01:55 +0700

    Added evaluation of polarity and objectivity, word counter.

--- evaluating_text.py
+++ evaluating_text.py
@@ -1,5 +1,20 @@
 from textblob import TextBlob
+import nltk
 
 text = input()
 text_blob_obj = TextBlob(text)
-print(text)
+
+polarity = text_blob_obj.sentiment.polarity
+objectivity = round((1 - text_blob_obj.sentiment.subjectivity) * 100, 1)
+
+if polarity > 0:
+    print('Тональность текста: положительная')
+elif polarity < 0:
+    print('Тональность текста: отрицательная')
+else:
+    print('Тональность текста: нейтральная')
+
+print(f'Объективность: {objectivity}%')
+print('Слов:', len(text_blob_obj.words))
+
+
