1f88cf6bce2cdf6ae53d08ed7de369bc95978622
Elizar54 <y.elizarev@g.nsu.ru>
2023-09-14 22:26:13 +0700

    added syllable counter, average length of the word in syllables.

--- evaluating_text.py
+++ evaluating_text.py
@@ -1,22 +1,33 @@
 from textblob import TextBlob
 from googletrans import Translator
 
-text = input()
+text = input().lower()
+word_list = text.split()
+syl_cnt = 0
 translator = Translator()
 detector = translator.detect(text)
+
 if detector.lang == 'ru':
     translation = translator.translate(text, src='ru', dest='en')
-    text = translation.text
-    print(text)
+    for letter in ['а', 'о', 'у', 'э', 'ы', 'и', 'е', 'ё', 'я', 'ю']:
+        syl_cnt += text.count(letter)
+
+    text_translated = translation.text
+
 elif detector.lang == 'en':
     pass
+
 else:
     print('Введенный текст не относится к русскому или английскому языкам.')
 
-text_blob_obj = TextBlob(text)
+text_blob_obj = TextBlob(text_translated)
 text_corrected = text_blob_obj.correct()
 polarity = text_corrected.sentiment.polarity
 objectivity = round((1 - text_corrected.sentiment.subjectivity) * 100, 1)
+
+if detector.lang == 'en':
+    for letter in ['a', 'e', 'i', 'o', 'u', 'y']:
+        syl_cnt += text_corrected.count(letter)
 
 print(text_corrected)
 
@@ -27,7 +38,7 @@ elif polarity < 0:
 else:
     print('Тональность текста: нейтральная')
 
+print(f'Средняя длина слова в слогах: {syl_cnt/len(word_list)}')
 print(f'Объективность: {objectivity}%')
-print('Слов:', len(text_corrected.words))
-
-
+print('Слов:', len(word_list))
+print('Количество слогов:', syl_cnt)
